3613d84bdac50c7c019ddca7f1a1115bb9020ec2
zWingz <zhengzwing@gmail.com>
2019-05-11 18:04:04 +0800

    fix: 🐛 export createObserver

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import Image from './lib/Image'
 import Preview, { PreviewApi } from './lib/ImgPreview'
+import { createObserver } from './lib/Image/observer'
 import './style/global.scss'
 export {
   Image,
   Preview,
-  PreviewApi
+  PreviewApi,
+  createObserver
 }
